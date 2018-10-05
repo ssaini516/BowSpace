240682afa13b874f7f21defc040a00b8959ec514
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-10-05 12:08:57 -0600

    show login form

--- BowSpace/bowspace/src/App.js
+++ BowSpace/bowspace/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ImageLogin from './components/LoginPage/ImageLogin.js';
+import LoginForm from './components/LoginPage/LoginForm';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <ImageLogin />
+        <LoginForm />
       </div>
     );
   }
