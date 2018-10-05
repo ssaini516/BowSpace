8506162382c82cc421835311d181f3ac393d10ad
sandeep saini <s.saini516@mybvc.ca>
2018-10-05 12:55:23 -0600

    adding left pannel bar

--- BowSpace/bowspace/src/App.js
+++ BowSpace/bowspace/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import ImageLogin from './components/LoginPage/ImageLogin.js';
+import LeftNavigationBar from './components/HomePage/LeftNavigationBar.js';
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <ImageLogin />
+        <LeftNavigationBar />
       </div>
     );
   }
--- BowSpace/bowspace/src/components/HomePage/LeftNavigationBar.js
+++ BowSpace/bowspace/src/components/HomePage/LeftNavigationBar.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 
-class LeftNavigationBar extends Component
-{
 
-    render(){
-        return
-        (
-            console.log("welcome")
-        );
+class LeftNavigationBar extends Component {
+    render() {
+      return (
+        <div className="left-panel">
+            <div class = "row">
+                <div class = "col-sm-8">
+                    <h2>user name</h2>
+                </div>
+            </div>
+            <div class = "row">
+                <div class = "col-sm-8">
+                <input type="text" class="form-control" name="search-list" />
+                </div>
+            </div>
+        </div>
+      );
     }
-}
-
-export default LeftNavigationBar
+  }
+export default LeftNavigationBar;
