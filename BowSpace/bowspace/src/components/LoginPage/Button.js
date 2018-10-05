c94bb68f2c6655cdce715f43ff1416dda5b30145
Parvir Sandhu <p.sandhu325@mybvc.ca>
2018-10-05 11:54:58 -0600

    Adding Sign in  Button

---
+++ BowSpace/bowspace/src/components/LoginPage/Button.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Button extends Component
+{
+    render(){
+        return(
+            <div>
+                <button type = "button" className = "btn btn-primary btn-lg">Sign in</button> 
+            </div>
+        );
+        
+    }
+}
+export default Button
