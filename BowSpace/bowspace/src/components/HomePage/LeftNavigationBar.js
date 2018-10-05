74faf30648eea22d6eb9623e45dafe14433f2282
sandeep saini <s.saini516@mybvc.ca>
2018-10-05 11:20:00 -0600

    Adding left bar component

---
+++ BowSpace/bowspace/src/components/HomePage/LeftNavigationBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LeftNavigationBar extends Component
+{
+
+    render(){
+        return
+        (
+            console.log("welcome")
+        );
+    }
+}
+
+export default LeftNavigationBar
