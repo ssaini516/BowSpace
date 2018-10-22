d5f2a4c1ce8ac19c987e9fedea254fb8bfb6acf2
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-10-22 12:36:15 -0600

    Fixed footer website

--- BowSpace/bowspace/src/components/HomePage/Footer.js
+++ BowSpace/bowspace/src/components/HomePage/Footer.js
@@ -7,10 +7,10 @@ class Footer extends Component {
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 copyright">
-                    Copy right © 2018
+                    Copyright ©2018
                     </div>
                     <div class="col-md-4 col-md-offset-6 attribution">
-                    Developer by  <a target="_blank" href="#">Reactnators</a> .
+                     Developed by  <a target="_blank" href="#">Reactnators</a> .
                     </div>
                 </div> 
             </div>
