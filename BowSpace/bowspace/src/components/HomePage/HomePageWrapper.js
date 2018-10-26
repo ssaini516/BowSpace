4250a1964d803de540cbf8aa155d3dba2fe17323
hai_do_488 <h.do488@mybvc.ca>
2018-10-26 07:12:31 -0600

    Changing HomeWrapper func to be a class

--- BowSpace/bowspace/src/components/HomePage/HomePageWrapper.js
+++ BowSpace/bowspace/src/components/HomePage/HomePageWrapper.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import LeftNavigationBar from './LeftNavigationBar.js';
 import ListPostsOfUsers from './ListPostsOfUsers.js';
@@ -7,44 +7,52 @@ import Footer from './Footer.js';
 import './HomePage.css';
 
 
-const HomePageWrapper = () => {
+class HomePageWrapper extends Component {
 
-    return(
-        <div id="home-page-wrapper" className="container-fluid mt-3">
-        
-            <div className="row">
-                <div className="col-md-4 col-lg-3">
-                    <LeftNavigationBar />
-                </div>
+    
 
-                <div className="col-md-1 col-lg-1">
-                    
-                </div>
+    ///
+    /// Render
+    ///
+    render() {
 
-                <div className="col-md-6 col-lg-7">
-                    <ListPostsOfUsers />
-                </div>
-            </div> 
+        return(
+            <div id="home-page-wrapper" className="container-fluid mt-3">
+            
+                <div className="row">
+                    <div className="col-md-4 col-lg-3">
+                        <LeftNavigationBar />
+                    </div>
 
-            <div className="row mb-4">
-                <div className="col-md-9 col-lg-9">
-                    
+                    <div className="col-md-1 col-lg-1">
+                        
+                    </div>
+
+                    <div className="col-md-6 col-lg-7">
+                        <ListPostsOfUsers />
+                    </div>
+                </div> 
+
+                <div className="row mb-4">
+                    <div className="col-md-9 col-lg-9">
+                        
+                    </div>  
+
+                    <div className="col-md-2 col-lg-2 mt-3">
+                        <Link to="/createPost">
+                            <Button type="primary">Create New Post</Button>
+                        </Link>                      
+                    </div>  
+
                 </div>  
 
-                <div className="col-md-2 col-lg-2 mt-3">
-                    <Link to="/createPost">
-                        <Button type="primary">Create New Post</Button>
-                    </Link>                      
-                </div>  
-
-            </div>  
-
-            <div className="row">
-                <div className="col-md-6 col-lg-11">
-                    <Footer />
-                </div>   
-            </div>          
-        </div>
-    );
+                <div className="row">
+                    <div className="col-md-6 col-lg-11">
+                        <Footer />
+                    </div>   
+                </div>          
+            </div>
+        );
+    }
 }
 export default HomePageWrapper;
