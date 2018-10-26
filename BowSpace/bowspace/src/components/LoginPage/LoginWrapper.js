36fd62d3975ad8e4ba7f45720e0cb2ec23d5e3b7
hai_do_488 <h.do488@mybvc.ca>
2018-10-26 11:40:52 -0600

    Convert LoginWrapper to class and add state for username and password

--- BowSpace/bowspace/src/components/LoginPage/LoginWrapper.js
+++ BowSpace/bowspace/src/components/LoginPage/LoginWrapper.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ImageLogin from './ImageLogin.js';
 import LoginForm from './LoginForm.js';
 import Button from '../Button/Button.js';
@@ -6,19 +6,30 @@ import { Link } from 'react-router-dom';
 import './Loggin-page.css';
 
 
-const LoginWrapper = () => {
+class LoginWrapper extends Component {
 
-    return (
-        <div className="login-wrapper">
-            <ImageLogin />
-            <LoginForm />
-            <Link to="/home">
-                <Button id="btn-login" type="success">
-                    Sign In <i className="fa fa-sign-in" aria-hidden="true"></i>
-                </Button>
-            </Link>        
-        </div>
-    );
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className="login-wrapper">
+                <ImageLogin />
+                <LoginForm />
+                <Link to="/home">
+                    <Button id="btn-login" type="success">
+                        Sign In <i className="fa fa-sign-in" aria-hidden="true"></i>
+                    </Button>
+                </Link>        
+            </div>
+        )
+    };
 }
 
 export default LoginWrapper;
