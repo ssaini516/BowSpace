42e4c685375c602708a5e89516f59a54a957ed9e
hai_do_488 <h.do488@mybvc.ca>
2018-10-27 21:59:00 -0600

    Display users who matching search keywords on left bar

--- BowSpace/bowspace/src/components/HomePage/LeftNavigationBar.js
+++ BowSpace/bowspace/src/components/HomePage/LeftNavigationBar.js
@@ -15,76 +15,99 @@ class LeftNavigationBar extends Component {
         this.props.logoutStatus(true);
     }
 
+    getUsersMatchingSearch = (loginToken, searchKeyWords) => {
+
+        let URL = 'http://api.bowspace.ca/rest/users';
+
+        // Build header obj 
+        const header = new Headers({
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Bearer ' + loginToken
+        });
+
+        const params = '?userid=0&keywords=' + searchKeyWords;
+
+        // Build request bosy
+        let requestBody = {
+            Method: 'GET',
+            headers: header,
+            cache: 'no-cache',
+            mode: 'cors',
+            credentials: 'omit',
+            redirect: 'error'
+        }
+
+        fetch(URL + params, requestBody)
+           .then(response => response.json())
+           .then(data => {
+               const listMatchingUsers = data.MatchingUsers;
+
+               if (listMatchingUsers !== null) {
+                   this.props.searchUsers(listMatchingUsers);
+               }       
+           })          
+    }
+
     handleSearchUser = () => {
     
         // get searching keyword
         const searchingUserInput = this.refs.searchUser.value;
         
-        // Call API to get matching users
-       
+        // Call API to get matching users  
+        let loginToken = sessionStorage.getItem('loginToken');
 
-            let loginToken = sessionStorage.getItem('loginToken');
-
-            console.log(loginToken);
-
-            let URL = 'http://api.bowspace.ca/rest/users';
-
-            const header = new Headers({
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Bearer ' + loginToken
-            });
-
-            const params = '?userid=0&keywords=';
-
-            let requestBody = {
-                Method: 'GET',
-                headers: header,
-                cache: 'no-cache',
-                mode: 'cors',
-                credentials: 'omit',
-                redirect: 'error'
-            }
-
-            fetch(URL + params, requestBody)
-                .then(response => response.json())
-                .then(dataList => console.log(dataList))
-               
-        
+        // Invoke function to get matching user from API
+        this.getUsersMatchingSearch(loginToken, searchingUserInput);                  
     }
 
     render() {
-      return (
-        <div className="card">
-            <div className="card-header text-white bg-primary">
-                <h5><i className="fa fa-user-circle-o"></i> {`Welcome ${this.loginedUsername}`}</h5>
-                <button className="btn btn-danger mt-4"
-                        onClick={this.handleLogout}>
-                    Logout
-                </button>
-            </div>
-            <div className="card-body">
-                <div className="input-group mb-3">
-                    <input type="text" 
-                           className="form-control" 
-                           placeholder="Username"
-                           ref="searchUser"
-                           />
-                </div>
 
-                <button className="btn btn-primary"
-                        onClick={this.handleSearchUser}>
-                    Search
-                </button>
-                
-                <p></p>
-                <ul className ="list-group">
-                    <li className ="list-group-item"><i className ="fa fa-user-circle-o"></i> Hai <i className ="fa fa-circle"></i></li>
-                    <li className ="list-group-item"><i className ="fa fa-user-circle-o"></i> Sandeep <i className ="fa fa-circle"></i></li>
-                    <li className ="list-group-item"><i className ="fa fa-user-circle-o"></i> Pablo <i className ="fa fa-circle"></i></li>
-                </ul>
+        const listUsers = this.props.listUsers;
+
+        return (
+            <div className="card">
+                <div className="card-header text-white bg-primary">
+                    <h5><i className="fa fa-user-circle-o"></i> {`Welcome ${this.loginedUsername}`}</h5>
+                    <button className="btn btn-danger mt-4"
+                            onClick={this.handleLogout}>
+                        Logout
+                    </button>
+                </div>
+                <div className="card-body">
+                    <div className="input-group mb-3">
+                        <input type="text" 
+                                className="form-control" 
+                                placeholder="Username"
+                                ref="searchUser"
+                                />
+                    </div>
+
+                    <button className="btn btn-primary"
+                            onClick={this.handleSearchUser}>
+                        Search
+                    </button>
+                    
+                    <p></p>
+                    <ul className ="list-group">
+
+                        {
+                            listUsers.map(
+                                (user, index) => {
+                                    return <li key={index}
+                                               className="list-group-item"
+                                            >
+                                                <i className="fa fa-user-circle-o"></i> 
+                                                    <span> {user.UserName} </span> 
+                                                <i className="fa fa-circle"></i>
+                                            </li>
+                                }
+                            )
+                        }
+
+                    </ul>
+                </div>
             </div>
-        </div>
-      );
+        );
     }
   }
 export default LeftNavigationBar;
