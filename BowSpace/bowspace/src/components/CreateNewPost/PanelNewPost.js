608cb1eed28d31ac2f0c8e865b20fa0caa1a0b43
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2018-10-30 19:43:04 -0600

    Added fully responsive layout

--- BowSpace/bowspace/src/components/CreateNewPost/PanelNewPost.js
+++ BowSpace/bowspace/src/components/CreateNewPost/PanelNewPost.js
@@ -62,69 +62,72 @@ class PanelPost extends Component {
         let listUserNamesInfo = this.listOfUsersInfo;
 
         return( 
-            <div id="wrapper-panel">
-                <div className="container">
-                    <div className="row"> 
-                        <div className="form-group mb-4 col-12">
-                            <div className="input-group-prepend">
-                                <h3 id="text-panel">BowSpace - create post</h3>
-                                
-                            </div>
+
+                <div className="container" >                   
+                     <div className="row"> 
+                        <div className="col-sm-0 col-md-0 col-lg-3">
                         </div>
 
-                        <div className="form-group mb-5 col-12">
-                            <div className="input-group-prepend">
+                        <div className="col-sm-12 col-md-12 col-lg-6" id="wrapper-panel">
+                            <div className="form-group mb-4  mt-3">
                                 <div className="input-group-prepend">
-                                    <span className="input-group-text"><i className="fa fa-buysellads"></i> Sender</span>
+                                    <h3 id="text-panel">BowSpace - create post</h3>
+                                    
                                 </div>
-                                <input className="form-control" ref="sender" value={this.userID} readOnly></input>
                             </div>
-                        </div>
-
-                        <div className="form-group mb-5 col-12">
-                            <div className="input-group-prepend">
+                            <div className="form-group mb-5">
                                 <div className="input-group-prepend">
-                                <span className="input-group-text"><i className="fa fa-user-plus"></i> Receipient</span>
+                                    <div className="input-group-prepend">
+                                        <span className="input-group-text"><i className="fa fa-buysellads"></i> Sender</span>
+                                    </div>
+                                    <input className="form-control" ref="sender" value={this.userID} readOnly></input>
                                 </div>
-                                <select className="form-control" ref="recipient" required>
-                                    {
-                                        (listUserNamesInfo) ? 
-                                            listUserNamesInfo.map(
-                                                (userName, index) => {
-                                                    return <option key={index} 
-                                                                value={userName.UserId}>
-                                                                {userName.UserName}
-                                                            </option>
-                                                }
-                                            ) : null
-                                    }
-                                </select>
-                            </div> 
-                        </div>
-
-                        <div className="input-group mb-5 col-12">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text"><i className="fa fa-comments"></i>Post content</span>
                             </div>
-                            <textarea ref="textarea" className="form-control" aria-label="With textarea" id="textarea" required></textarea>
-                        </div>
-                    </div>
-                    
-                    <div className="row">
-                        <div className="col-md-6">
 
-                        </div>
-                        <div className="col-md-6 mb-3">
+                            <div className="form-group mb-5">
+                                <div className="input-group-prepend">
+                                    <div className="input-group-prepend">
+                                    <span className="input-group-text"><i className="fa fa-user-plus"></i> Receipient</span>
+                                    </div>
+                                    <select className="form-control" ref="recipient" required>
+                                        {
+                                            (listUserNamesInfo) ? 
+                                                listUserNamesInfo.map(
+                                                    (userName, index) => {
+                                                        return <option key={index} 
+                                                                    value={userName.UserId}>
+                                                                    {userName.UserName}
+                                                                </option>
+                                                    }
+                                                ) : null
+                                        }
+                                    </select>
+                                </div> 
+                            </div>
+
+                            <div className="input-group mb-5">
+                                <div className="input-group-prepend">
+                                    <span className="input-group-text"><i className="fa fa-comments"></i>Post content</span>
+                                </div>
+                                <textarea ref="textarea" className="form-control" aria-label="With textarea" id="textarea" required></textarea>
+                            </div>
+
+                            
                             <Link to="/home">
-                            <button onClick={this.CreateNewPost} type="submit" className="btn btn-success btn-sm" id="btn-post">Post <i className="fa fa-spinner" aria-hidden="true"></i></button>
+                                <button onClick={this.CreateNewPost} type="submit" className="btn btn-success btn-sm" id="btn-post">Post <i className="fa fa-spinner" aria-hidden="true"></i></button>
                             </Link>
                             <Link to="/home">
-                                <button className="btn btn-danger btn-sm ml-2 mb-1" id="btn-cancel">Cancel <i className="fa fa-arrow-circle-left" aria-hidden="true"></i></button>       
+                                <button className="btn btn-danger btn-sm ml-2" id="btn-cancel">Cancel <i className="fa fa-arrow-circle-left" aria-hidden="true"></i></button>       
                             </Link>                         
-                        </div>                
+                        </div>
+
+                        <div className="col-sm-0 col-md-0 col-lg-3">
+                        </div>
+                        
                     </div>
-                </div>
+                    
             </div>
+            
         );
     }
 }
